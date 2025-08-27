f2ae4b2c036ce2be748afa3531b518ffa107788b
Felipe <felipe.bernardi@estudiantes.utec.edu.uy>
2025-08-27 20:40:39 -0300

    a

---
+++ ProyectoAerolineas/src/Logica/Logica/Pasajero.java
@@ -0,0 +1,14 @@
+package Logica;
+
+public class Pasajero {
+    private String nombre;
+    private String apellido;
+
+    public Pasajero(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+}
---
+++ ProyectoAerolineas/src/Presentacion/guiSesion/HacerReserva.java
@@ -0,0 +1,13 @@
+package guiSesion;
+
+import javax.swing.*;
+
+public class HacerReserva {
+    private JPanel panelDeCiudad;
+    private JTextField campoCiudad;
+    private JLabel Ciudad;
+    private JLabel Pais;
+    private JTextField campoPais;
+    private JButton crearButton;
+    private JButton cancelarButton;
+}
